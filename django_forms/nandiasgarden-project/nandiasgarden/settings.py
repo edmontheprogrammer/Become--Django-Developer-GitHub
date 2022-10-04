b0a8395494e4794536e2aba15d39831c97f8de78
Edmon the Programmer <edmontheprogrammer@gmail.com>
2022-10-04 08:19:10 +0400

    django froms final final commit
    django froms final final commit

--- django_forms/nandiasgarden-project/nandiasgarden/settings.py
+++ django_forms/nandiasgarden-project/nandiasgarden/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4hgu!ox2mc*ntty5bz3i8-s448qg5c6)*6)pbyw)!8w__r-y@q'
+# SECRET_KEY = 'nothing'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
